7483f52ab39964d8b9d6936650aba3df92223cc4
Karan Sharma <SHKA6013@MUHCAD.MUHCFRD.CA>
2020-09-15 12:47:38 -0400

    Adding proj files

---
+++ src/test/java/guru/springframework/karanpetclinic/KaranPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.karanpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaranPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
